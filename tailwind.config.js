79886bf8b083ccba4e0f467e18ca995482add6ae
SardorQobilov <sardorqobilov555@gmail.com>
2024-03-13 09:48:33 +0500

    header top section finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ["./**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				"32": "32px"
+			},
+			fontFamily: {
+				"SF-Pro-Display": ["SF Pro Display", 'sans-serif'],
+			},
+			lineHeight: {
+				"8.5": "35.2px"
+			}
+		},
 	},
 	plugins: [],
 }
